1ebed1c4f533e4f419e8b2c9a0173536e3b8dda5
abhishekasthana-251 <aasthana.ai@gmail.com>
2025-08-31 13:41:52 +0530

    Time: 1 ms (59.78%), Space: 8.7 MB (40.82%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -5,15 +5,13 @@ public:
             return false;
         }
         int y=x;
-        int revnum=0;
+        long long revnum=0;
         while(y!=0){
             int ld=y%10;
             revnum=(revnum*10)+ld;
             y=y/10;
         }
-        if(revnum>INT_MAX || revnum<INT_MIN){
-            return false;
-        }
+        
         if(revnum==x){
             return true;
         }
