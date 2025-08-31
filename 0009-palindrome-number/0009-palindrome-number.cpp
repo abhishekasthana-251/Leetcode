b72441a481b89cf8086d38276ae7412c57be531b
abhishekasthana-251 <aasthana.ai@gmail.com>
2025-08-31 13:50:34 +0530

    Time: 4 ms (32.4%), Space: 8.8 MB (25.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
+        
         int y=x;
         long long revnum=0;
-        while(y!=0){
+        while(y>0){
             int ld=y%10;
             revnum=(revnum*10)+ld;
             y=y/10;
