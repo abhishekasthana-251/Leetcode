76057f1851f80408b92a25805f7cb60851333c2d
Abhishek <aasthana.ai@gmail.com>
2025-09-20 00:14:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;
+        int j=1;
+        while(i<j && j<n){
+            if(nums[i]==0 && nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+            }
+            if(nums[j]==0){
+                j++;
+            }
+        }
+        
+    }
+};
