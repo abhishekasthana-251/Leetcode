d2de4c784ef74a0c2b077fb641e532082ed255f8
Abhishek <aasthana.ai@gmail.com>
2025-09-23 23:57:35 +0530

    Time: 0 ms (100%), Space: 18.6 MB (78.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,20 +5,44 @@ public:
         int m=matrix.size();
         int n =matrix[0].size();
 
-        vector<vector<int>> temp=matrix;
-        //0(m*n*(n+m))
+        // vector<vector<int>> temp=matrix;
+        // //0(m*n*(n+m))
+        // for(int i =0;i<m;i++){
+        //     for(int j= 0;j<n;j++){
+        //         if(matrix[i][j]==0){
+        //             for(int k=0;k<n;k++){
+        //                 temp[i][k]=0; //making ith row as 0
+        //             }
+        //             for(int k=0;k<m;k++){
+        //                 temp[k][j]=0;//making jth column as 0
+        //             }
+        //         }
+        //     }
+        // }
+        // matrix=temp;
+
+
+
+        //solving it into m+n
+
+        vector<bool>row(m,false);
+        vector<bool>col(n,false);
+
         for(int i =0;i<m;i++){
             for(int j= 0;j<n;j++){
                 if(matrix[i][j]==0){
-                    for(int k=0;k<n;k++){
-                        temp[i][k]=0; //making ith row as 0
-                    }
-                    for(int k=0;k<m;k++){
-                        temp[k][j]=0;//making jth column as 0
-                    }
+                    row[i]=true;
+                    col[j]=true;
+                }
+             }
+         }
+
+         for(int i =0;i<m;i++){
+             for(int j= 0;j<n;j++){
+                if(row[i]==true || col[j]==true){
+                    matrix[i][j]=0;
                 }
             }
         }
-        matrix=temp;
     }
 };
