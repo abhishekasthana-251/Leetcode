1db17fa175bf2719e4d8f0281623ed9294e5cfd0
Abhishek <aasthana.ai@gmail.com>
2026-04-02 17:07:56 +0530

    Time: 7 ms (56.74%), Space: 26.9 MB (15.73%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,7 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #first reverse the n element (7,6,5,4,3,2,1)
+        #second reverse the fisrt k element(5,6,7,4,3,2,1)
+        #third reverse the n-k element (5,6,7,1,2,3,4)
+
         n=len(nums)
         k=k%n
-
-        nums[:]=nums[-k:]+nums[:-k]
+        def reverse(start: int, end:int) -> None:
+            while(start<end):
+                nums[start],nums[end]=nums[end],nums[start]
+                start+=1
+                end-=1
+            
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)
